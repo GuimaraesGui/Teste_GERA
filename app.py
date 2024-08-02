ba64ed351ad02e344369daf9a2ef10798d95222f
GuimaraesGui <91296034+GuimaraesGui@users.noreply.github.com>
2024-08-02 15:48:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,57 @@
 import win32com.client as win32
 import base64
 
-# Lista de contatos
-contatos_GN = ["guimaraes_gui@outlook.com.br;tripaseca.kun@gmail.com;ep.0.season.0@gmail.com;vicktoriagranger15@gmail.com;equipeotaku.kun@gmail.com"]
+# Listas de contatos
+contatos_GN1 = ["e-mail 1",
+                "e-mail 2",
+                "e-mail 3"]
 
-contatos_GN1 = [["guimaraes_gui@outlook.com.br"],["tripaseca.kun@gmail.com"],
-                ["ep.0.season.0@gmail.com"], ["vicktoriagranger15@gmail.com"],
-                  ["equipeotaku.kun@gmail.com"]]
+# Lista com os anexos dos consumos de gás natural dos clientes
+caminho_anexos = [
+        r"C:\Users\00805129\OneDrive - NATURGY INFORMATICA S.A\Documentos\02 - Industrial\Qualidade GNV\CFQ - SPS\01 - JANEIRO\Diário\Consumo Ajinomoto Janeiro.xlsx",
+        r"C:\Users\00805129\OneDrive - NATURGY INFORMATICA S.A\Documentos\02 - Industrial\Qualidade GNV\CFQ - SPS\01 - JANEIRO\Diário\Consumo CBA Janeiro.xlsx",
+        r"C:\Users\00805129\OneDrive - NATURGY INFORMATICA S.A\Documentos\02 - Industrial\Qualidade GNV\CFQ - SPS\01 - JANEIRO\Diário\Consumo DeNora Janeiro.xlsx"
+        ]
 
-# Caminho da imagem
+# Caminho da imagem da minha assinatura
 caminho_imagem = "C:\\Users\\00805129\\OneDrive - NATURGY INFORMATICA S.A\\Escritorio\\Studying_Python\\E-mail_automation\\ass_email.png"
 
-# Criar a integração com o Outlook
+# Criando a integração com o Outlook
 outlook = win32.Dispatch('outlook.application')
 
-# Criar um e-mail
-email = outlook.CreateItem(0)
+# Loop sobre os contatos
+for i in range(len(contatos_GN1)):
+    # Criar um e-mail
+    email = outlook.CreateItem(0)
 
-# Configurar as informações do e-mail
-email.To = ";".join(contatos_GN)
-email.Subject = "E-mail automático do Python"  # Assunto
+    # Configurar as informações do e-mail para o contato atual
+    email.To = contatos_GN1[i]  # Substitua pelo contato atual
+    email.Subject = f"E-mail automático do Python para {contatos_GN1[i]}"  # Assunto
 
-# Ler o conteúdo da imagem e convertê-lo para base64
-with open(caminho_imagem, "rb") as img_file:
-    img_data = img_file.read()
-    img_base64 = base64.b64encode(img_data).decode("utf-8")
+    # Ler o conteúdo da imagem e convertê-lo para base64
+    with open(caminho_imagem, "rb") as img_file:
+        img_data = img_file.read()
+        img_base64 = base64.b64encode(img_data).decode("utf-8")
 
-# Incorporar a imagem diretamente no HTMLBody
-email.HTMLBody = f"""
-<p>Boa tarde a todos,</p>
+    # Incorporar o anexo diretamente no HTMLBody
+    email.HTMLBody = f"""
+    <p>Boa tarde {contatos_GN1[i]},</p>
 
-<p>Anexado, segue atualização a respeito da composição físico-química.</p>
+    <p>Anexado, segue atualização a respeito da composição físico-química.</p>
 
-<p>Aqui está uma imagem incorporada:</p>
+    <p>At.te,</p>
+    <img src="data:image/png;base64,{img_base64}">
+    """
 
-<p>At.te,</p>
-<img src="data:image/png;base64,{img_base64}">
+    # Adicionar o anexo ao e-mail
+    attachment = email.Attachments.Add(Source=caminho_anexos[i])
+
+    # Enviar o e-mail
+    email.Send()
+
+"""
+for i in range(len(contatos_GN1)):
+    print(contatos_GN1[i], end=" / ")
 """
 
-# Enviar o e-mail
-email.Send()
-
-print("\nEmail Enviado")
+print("\nE-mails Enviados")
